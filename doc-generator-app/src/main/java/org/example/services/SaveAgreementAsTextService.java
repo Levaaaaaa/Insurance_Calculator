4bcb77c45c751fd720163a0a51d8c45a8f2d1972
Levaaaaaa <levasneshko@tut.by>
2025-02-26 23:52:21 +0300

    write agreement from queue into unique file

--- doc-generator-app/src/main/java/org/example/services/SaveAgreementAsTextService.java
+++ doc-generator-app/src/main/java/org/example/services/SaveAgreementAsTextService.java
@@ -5,8 +5,14 @@ import org.example.dto.AgreementDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+import java.util.Collections;
 
 @Service
 public class SaveAgreementAsTextService implements DocGeneratorService{
@@ -15,9 +21,11 @@ public class SaveAgreementAsTextService implements DocGeneratorService{
 
     @Override
     public void generateDocument(AgreementDTO agreementDTO) throws IOException{
-        try (FileWriter writer = new FileWriter(path + "/agreement.txt")) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            writer.write(objectMapper.writeValueAsString(agreementDTO));
+        Path file = Path.of(String.format("%s/agreement-proposal-%s.txt", path, agreementDTO.getUuid().toString()));
+        ObjectMapper objectMapper = new ObjectMapper();
+        //Files.write(file, objectMapper.writeValueAsString(agreementDTO).getBytes(), StandardOpenOption.CREATE);
+        try (FileWriter fileWriter = new FileWriter(file.toFile())) {
+            fileWriter.write(objectMapper.writeValueAsString(agreementDTO));
         }
     }
 }
